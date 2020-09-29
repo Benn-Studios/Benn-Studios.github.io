0fc16c173a9106d9899ac73f63e4a27312f3e86c
Benn-Studios <72105550+Benn-Studios@users.noreply.github.com>
2020-09-29 22:41:09 +0100

    Honestly

---
+++ assets/javascript/app.js
@@ -0,0 +1,60 @@
+// Made by Ben - https://ben-bryant.xyz/ | https://github.com/Benn-Gold | Discord: Bwen#4133
+
+
+var _app = function () {
+    this.id = 0;
+    this.videoElement = null;
+    this.audioElement = null;
+    this.musicVolume = 0.12;
+    this.musicFadeIn = 4000;
+    this.skippedIntro = false;
+    this.backgroundToggler = false;
+    this.shouldIgnoreVideo = false;
+    this.effects = ["bounce", "flash", "pulse", "rubberBand", "shake", "swing", "tada", "wobble", "jello"];
+    this.brandDescription = [
+        "Ben Bryant", 
+        "Free Lance Developer", 
+        "Ben Gold", 
+        "Bwen Gold"
+    ];
+
+    this.titleChanger = function (text, delay) {
+        text = text ||
+            ['Ben', 'Gold', 'Bwen Gold', 'Ben Bryant', 'Developer', 'ben-bryant.xyz', 'Song name:LiL Peep - Lil Jeep'];
+        delay = delay || 2000;
+
+        var counter = 0;
+
+        setInterval(function () {
+            if(counter < text.length) 
+                document.title = text[counter++]; 
+            else 
+                document.title = text[counter = 0];
+        }, delay);
+    }
+
+    this.iconChanger = function (urls, delay) {
+        if (!urls)
+            return;
+
+        delay = delay || 2000;
+
+        var counter = 0;
+        
+        setInterval(function () {
+            if(counter < urls.length) {
+                var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+                link.type = 'image/x-icon';
+                link.rel = 'shortcut icon';
+                link.href = urls[counter];
+                document.getElementsByTagName('head')[0].appendChild(link);
+            }
+            else 
+                counter = 0;
+
+           ++counter;
+        }, delay);
+    }
+};
+
+var app = new _app();
